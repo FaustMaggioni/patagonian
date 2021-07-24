3698f428088dc6e3034615dc15871026ad41e9bf
FaustMaggioni <faustino.maggioni@est.fi.uncoma.edu.ar>
2021-07-24 15:42:12 -0300

    preguntar color

--- src/components/DefaultButton/DefaultButton.tsx
+++ src/components/DefaultButton/DefaultButton.tsx
@@ -5,14 +5,19 @@ import styles from './styles';
 interface Props {
    onPress: () => void;
    label: string;
+   color: string;
 };
 
-const DefaultButton = ({ onPress, label }: Props) => {
+const DefaultButton = ({ onPress, label, color }: Props) => {
    return (
       <TouchableOpacity onPress={onPress} color='red' style={styles.mainContainer}>
          <Text style={styles.textContainer}> {label} </Text>
       </TouchableOpacity>
    );
 };
+
+/*
+const createStyles = (color) => 
+   fondo: backgroundColor: color; */ //Preguntar
 
 export default DefaultButton;
